07b034bb540e05efaeb4571a3ee7d81585322386
rahul290998 <68946112+rahul290998@users.noreply.github.com>
2020-09-01 16:47:48 +0530

    Update filterLogs.groovy

--- vars/filterLogs.groovy
+++ vars/filterLogs.groovy
@@ -3,6 +3,23 @@
 import org.apache.commons.lang.StringUtils
 
 def call(String filter_string, int occurrence) {
+    pipeline{
+      agent any
+        stages{
+
+              stage('maven build'){
+                  steps{
+                      script{
+		    	                bat "mvn clean compile"
+                      	  }
+               	     }  
+                 }	
+        }
+}
+
+    
+    
+    
     def logs = currentBuild.rawBuild.getLog(10000).join('\n')
     int count = StringUtils.countMatches(logs, filter_string);
     if (count > occurrence -1) {
