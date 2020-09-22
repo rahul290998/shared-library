d992e0ced8aae7b0253f90b1ff629614f02343b7
rahul290998 <68946112+rahul290998@users.noreply.github.com>
2020-09-22 18:39:00 +0530

    Create deploy.groovy

---
+++ vars/deploy.groovy
@@ -0,0 +1,30 @@
+  
+#!/usr/bin/env groovy
+
+import org.apache.commons.lang.StringUtils
+
+def call(int number)
+{
+pipeline
+{
+agent any
+stages
+{
+stage('first step')
+{
+steps{
+bat"mvn clean install"
+}
+  
+}
+  stage('deploying')
+  {
+    steps{
+     deploy adapters: [tomcat8(credentialsId: '5866d9c5-7497-4a2f-9477-39037e355cb6', path: '', url: 'http://localhost:8083/')], contextPath: 'myproject', war: '**/*.war'
+}
+  }
+  
+}
+}
+
+}
