8189402bb4fd62b7723afec5a7d3b5bf0356bd7d
rahul290998 <68946112+rahul290998@users.noreply.github.com>
2020-09-23 12:35:39 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -9,11 +9,12 @@ stages
 {
   stage('deploying')
   {
-    steps{
-     deploy adapters: [tomcat8(credentialsId: stageParams.credentials, path: '', url: stageParams.patth)],
+    steps
+    {
+     deploy adapters: [tomcat8(credentialsId: '5866d9c5-7497-4a2f-9477-39037e355cb6', path: '', url:'http://localhost:8083/')],
        contextPath: stageParams.context, 
-       war: stageParams.warr
-}
+       war: stageParams.warr  
+    }
   }
   
 }
