b2bd278446df87010c9678dc5e94706192f9b378
rahul290998 <68946112+rahul290998@users.noreply.github.com>
2020-09-23 14:16:37 +0530

    Update and rename deploy.groovy to deploying.groovy

--- vars/deploying.groovy
+++ vars/deploying.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 import org.apache.commons.lang.StringUtils
-def call(LinkedHashMap stageParams)
+def call(Map stageParams)
 {
 pipeline
 {
@@ -11,7 +11,7 @@ stages
   {
     steps
     {
-     deploy adapters: [tomcat8(credentialsId: '5866d9c5-7497-4a2f-9477-39037e355cb6', path: '', url:'http://localhost:8083/')],
+     deploy adapters: stageParams.adapter,
        contextPath: stageParams.context, 
        war: stageParams.warr  
     }
