8a186e0ba8905330f1ecb5b41a40a5069b83addd
rahul290998 <68946112+rahul290998@users.noreply.github.com>
2020-09-23 11:43:04 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 import org.apache.commons.lang.StringUtils
-def call()
+def call(Map stageParams)
 {
 pipeline
 {
@@ -17,7 +17,9 @@ bat"mvn clean install"
   stage('deploying')
   {
     steps{
-     deploy adapters: [tomcat8(credentialsId: '5866d9c5-7497-4a2f-9477-39037e355cb6', path: '', url: 'http://localhost:8083/')], contextPath: 'myproject', war: '**/*.war'
+     deploy adapters: stageParams.adapter,
+       contextPath: stageParams.context, 
+       war: stageParams.warr
 }
   }
   
